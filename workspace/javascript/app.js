b9911be02bfa783754eecf42b759b6634acfd23f
giuseppe2048 <bauo81@gmail.com>
2017-11-15 21:53:34 +0100

    fine game theory parte 1

---
+++ workspace/javascript/app.js
@@ -0,0 +1,16 @@
+let a = [];
+a.push(1);
+a.push(2);
+for (let i of a){
+	console.log(`Test: ${i}`);
+}
+console.log('hello');
+
+let b = (x) => {
+	return x+1;
+};
+
+let c = (y) => {
+	return y-2;
+};
+
